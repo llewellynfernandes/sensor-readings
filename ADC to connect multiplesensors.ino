b8652ce3ce4e848628d1183052cc1903fda77a64
llewellynfernandes <llewellyn95@gmail.com>
2017-04-24 11:19:41 +0530

    Add files via upload

---
+++ ADC to connect multiplesensors.ino
@@ -0,0 +1,57 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+//Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+
+//CONNECTION OF ESP8266 TO ADS1115
+//VDD -->3.3 V
+//GND -->GND
+//SCL --> D1 --> GPIO 5
+//SDA --> D2 --> GPIO 4
+
+//ADS1115 is 16 bit.. But of this, 1st bit is used for sign, and remaining 15 bits only are used for data.
+//Therfore, maximum adc output is 2^15=32768
+//ADS1115 is to be used for maximum 6.144 V.. For 6.144 V, it gives maximum output of 2^15=32768
+//For 5V, maximum adc output is ~26193
+//For 3.3V, maximum adc output is ~17658
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+  
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin();
+}
+
+void loop(void) 
+{
+  int16_t adc0, adc1, adc2, adc3,adcesp;
+  adcesp=analogRead(A0);
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  Serial.print("ADC ESP: "); Serial.println(adcesp);
+  Serial.print("AIN0: "); Serial.println(adc0);
+  Serial.print("AIN1: "); Serial.println(adc1);
+  Serial.println(" ");
+  
+  delay(1000);
+}
+
+
